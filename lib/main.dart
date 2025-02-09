7be90c4a360b0e1cc1100e1afb8deae4230c4a9a
subhanlatki <subhanlatki@gmail.com>
2025-02-08 22:40:51 -0800

    folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: LoginScreen(),
+    debugShowCheckedModeBanner: false,
+     home: LoginScreen(), 
     );
   }
 }
